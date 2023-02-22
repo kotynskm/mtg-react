4621fca242fd5a39894a77207a9335dc8572d20d
Kailey <k.kotynski@gmail.com>
2023-02-22 15:21:26 -0800

    Refactored delete function, pass delete function to child component and update card state

--- src/Favorites.jsx
+++ src/Favorites.jsx
@@ -6,25 +6,33 @@ import Pagination from "./Pagination";
 const Favorites = () => {
   const [cards, setCards] = useState([]);
   const [currPage, setCurrPage] = useState(1);
-  const [cardsPerPage, setCardsPerPage] = useState(6);
+  const [cardsPerPage, setCardsPerPage] = useState(4);
 
   // fires a function when component is rendered - put [] as second argument so that it will only fire once when component first renders
+  const getCards = async () => {
+    // proxy in package.json will direct to our correct req route so we don't have to use the entire http rep, something like just "/api/cards"
+    const res = await fetch("http://localhost:3000/");
+    // parse json from the res object
+    const resJSON = await res.json();
+    console.log(resJSON.data);
+
+    // check we got the data back ok, if so then update cards
+    if (res.ok) {
+      setCards(resJSON.data);
+    }
+  };
   useEffect(() => {
-    const getCards = async () => {
-      // proxy in package.json will direct to our correct req route so we don't have to use the entire http rep, something like just "/api/cards"
-      const res = await fetch("http://localhost:3000/");
-      // parse json from the res object
-      const resJSON = await res.json();
-      console.log(resJSON.data);
-
-      // check we got the data back ok, if so then update cards
-      if (res.ok) {
-        setCards(resJSON.data);
-      }
-    };
-
     getCards();
   }, []);
+
+  const handleDelete = (databaseID) => {
+    fetch("http://localhost:3000/delete/" + databaseID, {
+      method: "POST",
+    })
+      .then((res) => res.text()) // or res.json()
+      .then((res) => console.log(res));
+    getCards();
+  };
 
   const lastIndex = currPage * cardsPerPage;
   const firstIndex = lastIndex - cardsPerPage;
@@ -39,8 +47,8 @@ const Favorites = () => {
             return <Card name={card.name} url={card.img} key={card._id} />;
           })} */}
         <Grid container>
-          {cards &&
-            cards.map((card) => {
+          {currCards &&
+            currCards.map((card) => {
               return (
                 <FavoriteCard
                   name={card.name}
@@ -48,15 +56,16 @@ const Favorites = () => {
                   id={card.id}
                   databaseID={card._id}
                   key={card._id}
+                  handleDelete={handleDelete}
                 />
               );
             })}
-          {/* <Pagination
+          <Pagination
             totalCards={cards.length}
             cardsPerPage={cardsPerPage}
             setCurrentPage={setCurrPage}
             currentPage={currPage}
-          /> */}
+          />
         </Grid>
       </div>
     </div>

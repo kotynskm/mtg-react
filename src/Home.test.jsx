5497e466e0bbeaab4e4c10f8728b416f189b17e7
Kailey <k.kotynski@gmail.com>
2023-03-09 14:49:06 -0800

    Added tests for home component

--- src/Home.test.jsx
+++ src/Home.test.jsx
@@ -4,7 +4,15 @@ import Home from "./Home";
 import App from "./App";
 
 it("should have welcome to MTG app", () => {
-  render(<App />);
+  render(<Home />);
   const message = screen.getByText(/welcome to mtg react app/i);
   waitFor(() => expect(message).toBeInTheDocument());
+});
+
+it("should be a heading element", () => {
+  render(<Home />);
+  const heading = screen.getByRole("heading", {
+    name: "Welcome to MTG React App",
+  });
+  waitFor(() => expect(heading).toBeInTheDocument());
 });

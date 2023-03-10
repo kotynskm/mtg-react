eab503b740568d7b931f269da8ae58415ebc09de
Kailey <k.kotynski@gmail.com>
2023-03-10 11:25:32 -0800

    Added describe blocks to tests

--- src/components/HitPoints/__test__/HitPoints.test.jsx
+++ src/components/HitPoints/__test__/HitPoints.test.jsx
@@ -2,8 +2,18 @@ import { render, screen } from "@testing-library/react";
 import { waitFor } from "@testing-library/react";
 import HitPoints from "../HitPoints";
 
-it("should render the correct amount of hitpoints", () => {
-  render(<HitPoints />);
-  const valueElement = screen.getAllByText(/20/i);
-  waitFor(() => expect(valueElement.length).toBe(2));
+describe("HitPoints", () => {
+  // check that the elements are displaying correct hitpoints
+  it("should render the correct amount of hitpoints", () => {
+    render(<HitPoints />);
+    const valueElement = screen.getAllByText(/20/i);
+    waitFor(() => expect(valueElement.length).toBe(2));
+  });
+
+  // check that element is visible to the user (test for potential style changes that could affect the element)
+  it("should be visible to the user", () => {
+    render(<HitPoints />);
+    const valueElement = screen.getAllByText(/20/i);
+    waitFor(() => expect(valueElement).toBeVisible());
+  });
 });

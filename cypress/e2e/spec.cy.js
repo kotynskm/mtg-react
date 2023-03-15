8717436f27e0dc5f643fef3f9c158c7180cbd2f9
Kailey <k.kotynski@gmail.com>
2023-03-15 13:38:06 -0700

    Edited test msg

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
 describe("My First Test", () => {
-  it('finds the content "type"', () => {
+  it("finds the welcome message", () => {
     cy.visit("http://127.0.0.1:5173/");
 
     cy.contains("Welcome to MTG React App");
